deba2d548b05f6793117719873a33532e1f278d9
asd15-pass <ymdr4210@gmail.com>
2023-08-02 17:08:38 +0900

    つぶやき本文と同時に、投稿日時を表示するようviewを修正してください。

--- laravel_theme/sample-app/resources/views/tweets/index.blade.php
+++ laravel_theme/sample-app/resources/views/tweets/index.blade.php
@@ -7,6 +7,14 @@
   </head>
   <body>
     <h1>つぶやき一覧</h1>
-    <p>{{ $name }}</p>
+
+    <div class="tweets">
+      @foreach($tweets as $tweet)
+        <div class="tweet">
+          <p>{{ $tweet->content }}</p>
+          <p>{{ $tweet->created_at}}</p>
+        </div>
+      @endforeach
+    </div>
   </body>
 </html>
