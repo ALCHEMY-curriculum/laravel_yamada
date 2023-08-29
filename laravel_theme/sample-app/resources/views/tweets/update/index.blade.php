561c99af940e9f7bac2410671d20640ea37bd89c
asd15-pass <ymdr4210@gmail.com>
2023-08-30 00:53:49 +0900

    修正漏れ修正

--- laravel_theme/sample-app/resources/views/tweets/update/index.blade.php
+++ laravel_theme/sample-app/resources/views/tweets/update/index.blade.php
@@ -1,12 +1,4 @@
-<!doctype html>
-<html lang="ja">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ついったー</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  </head>
-  <body>
+
     <x-layout>
     <div class="container">
       <h1>つぶやき編集 #{{ $tweet->id }}</h1>
@@ -40,5 +32,4 @@
 @endif
     </div>
     </x-layout>
-  </body>
-</html>
+ 
