ae50711082b6f1e7be8f4ba9231e207cfb00677a
asd15-pass <ymdr4210@gmail.com>
2023-08-30 00:05:51 +0900

    指摘箇所修正

--- laravel_theme/sample-app/resources/views/components/tweets.blade.php
+++ laravel_theme/sample-app/resources/views/components/tweets.blade.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   </head>
   <body>
-    {{ $slot }}
+  <p>{{ $tweet->content }}</p>
+<p>{{ $tweet->user->name }} {{ $tweet->created_at }}</p>
   </body>
 </html>
