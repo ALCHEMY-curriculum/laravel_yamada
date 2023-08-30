2e24a37f655b49dd98ede586ca2ac621d6a27f8e
asd15-pass <ymdr4210@gmail.com>
2023-08-30 22:21:34 +0900

    指摘箇所修正

--- laravel_theme/sample-app/resources/views/account/index.blade.php
+++ laravel_theme/sample-app/resources/views/account/index.blade.php
@@ -1,15 +1,4 @@
-<!doctype html>
-<html lang="ja">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>アカウントページ</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 
-  </head>
-  <body>
   <x-layout title="アカウントページ">
   <div class="container">
     <h1>アカウントページ</h1>
@@ -27,6 +16,5 @@
     </form>
   </div>
 </x-layout>
-  </body>
-</html>
+ 
 
